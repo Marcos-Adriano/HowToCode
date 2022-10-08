45039d1dddd4ef2d44ae84c8ef09f3a3ddf65469
Marcos-Adriano <marcosdevgoncalves@gmail.com>
2022-10-07 21:46:35 -0400

    mudanças feitas pelo professor

--- search.php
+++ search.php
@@ -5,13 +5,19 @@ include_once "config.php";
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width= , initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.css">
         <title>Lista de cursos</title>
-        <meta charset="UTF-8"></meta>
     </head>
-    <body>
-        <h1>Lista</h1>
+<html>
+    
         <table border="1">
                 <tr>
                     <th>ID</th>
@@ -47,4 +53,7 @@ include_once "config.php";
             
         </table>
     </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 </html>
