6f5fe835c96b95858fe16e9091c9698a683539ce
Marcos-Adriano <marcosdevgoncalves@gmail.com>
2022-10-12 14:43:48 -0400

    mensagem em listagem

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,21 +1,42 @@
 <?php
-// <td>'.$course->course_description.'</td> 
+
+    $mensagem = '';
+    if(isset($_GET['status'])){
+    switch ($_GET['status']) {
+        case 'success':
+        $mensagem = '<div class="alert alert-success">Ação executada com sucesso!</div>';
+        break;
+
+        case 'error':
+        $mensagem = '<div class="alert alert-danger">Ação não executada!</div>';
+        break;
+    }
+    }
+
     $results = '';
     foreach($courses as $course){
         $results .= '<tr>
                         <td>'.$course->course_id.'</td>
                         <td>'.$course->course_name.'</td>
                         <td>'.$course->course_instructor.'</td>
-                        
+                        <td>'.$course->course_description.'</td> 
                         <td>'.$course->course_numberClasses.'</td>
                         <td>'.$course->course_hours.'</td>
                         <td>'.($course->course_isActive == 1 ? 'Ativo' : 'Inativo').'</td>
                         <td><a href="editar.php?id='.$course->course_id.'"><button type="button" class="btn btn-primary">Editar</button></a> <a href="excluir.php?id='.$course->course_id.'"><button type="button" class="btn btn-danger">Excluir</button></a></td>
                         </tr>';
     }
+    $results = strlen($results) ? $results : '<tr>
+                                                <td colspan="7" class="text-center">
+                                                        Nenhum curso encontrado!
+                                                </td>
+                                            </tr>';
+
 ?>
 
 <main>
+
+    <?=$mensagem?>
 
     <section>
         <a href="cadastrar.php">
@@ -30,7 +51,7 @@
                     <th>ID</th>
                     <th>Nome</th>
                     <th>Instrutor</th>
-                    <!-- <th>Descrição</th> -->
+                    <th>Descrição</th>
                     <th>Quantidade de aulas</th>
                     <th>Carga horária</th>
                     <th>Ativo</th>
